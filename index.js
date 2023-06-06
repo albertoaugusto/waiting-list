d6d1b0b61fb444c18e94b05a1456fe64dadbce0e
Alberto Souza <albertosouza16.contato@gmail.com>
2023-06-06 14:40:27 -0300

    first part

---
+++ index.js
@@ -0,0 +1,10 @@
+const pacients = ["Letícia", "Murillo", "Victor"];
+    
+for(let i = 0; i < pacients.length; i++){
+    let list = pacients[i]
+    alert("Paciente Número: "+ i + " " + list)
+}
+
+let menu = alert(list)
+
+
