4311f2f7765801cbd33729971b423bd864e70c6d
Alberto Souza <albertosouza16.contato@gmail.com>
2023-06-06 15:51:03 -0300

    second part

--- index.js
+++ index.js
@@ -5,6 +5,23 @@ for(let i = 0; i < pacients.length; i++){
     alert("Paciente Número: "+ i + " " + list)
 }
 
-let menu = alert(list)
+let menu = prompt("Menu interativo, escolha uma opção: " + "\n\n" +
+            "1 - Novo Paciente" + "\n" +
+            "2 - Consultar Paciente" + "\n" +
+            "3 - Sair"
+)
+
+switch (menu){
+    case "1":
+        let pacient = prompt("Digite o nome do paciente: ")
+        pacients.push(pacient)
+        break
+    case "2":
+        alert("Paciente " + pacients[0])
+        pacients.shift()
+    case "3":
+        alert("Encerrando...")
+        break
+}
 
 
